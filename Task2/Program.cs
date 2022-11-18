830477ffce5d176ad4713eea9c8d6920ee1afad2
Ilya Ol <ilyaolegin@mail.ru>
2022-11-18 19:16:32 +0300

    Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введи 2 числа и узнай какое из них больше, а какое меньше");
+{
+ 
+        int a, b;
+ 
+        Console.WriteLine("Введите два числа : ");
+ 
+        a = Convert.ToInt32(Console.ReadLine());
+ 
+        b = Convert.ToInt32(Console.ReadLine());
+ 
+        if (a > b)
+ 
+        {
+ 
+            Console.WriteLine("{0} - наибольшее число, {1} - наименьшее число", a, b);
+ 
+        }
+ 
+        else
+ 
+        {
+ 
+             Console.WriteLine("{0} - наибольшее число, {1} - наименьшее число", b, a);
+ 
+        }
+ 
+        Console.ReadLine();
+ 
+    }
+   
