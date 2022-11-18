ec7d0009bb82042bb3fa5b01f50649ba2a590d75
Ilya Ol <ilyaolegin@mail.ru>
2022-11-18 20:03:59 +0300

    Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введи число и узнай является ли оно четным");
+int num; 
+    Console.Write("Введите число : ");
+    num = int.Parse(Console.ReadLine());
+    if (num % 2 == 0)
+    {
+        Console.Write("Введенное число является четным");
+        Console.Read();
+    }        
+        else
+        { 
+            Console.Write("Введенное число нечетное");
+            Console.Read();
+        }    
